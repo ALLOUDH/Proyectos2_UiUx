286ec26e12ee6b808621a1eb644097afda261038
Hector Chavez <48160099+ALLOUDH@users.noreply.github.com>
2023-09-24 11:12:24 -0500

    Implementacion de TextInputs y Checkbox funcional

---
+++ JavaScript/showpass.js
@@ -0,0 +1,8 @@
+function mostrar_pass(){
+    var tipo = document.getElementById("password");
+    if(tipo.type == 'password'){
+        tipo.type = 'text';
+    }else{
+        tipo.type = 'password';
+    }
+}
