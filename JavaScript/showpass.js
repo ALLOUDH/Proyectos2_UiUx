3f27e73d29ed71117628758c796f656f44105177
Layoned <75380573@continental.edu.pe>
2023-09-27 15:58:48 -0500

    Login interactivo, que permite los ingresos de lo solicitado

--- JavaScript/showpass.js
+++ JavaScript/showpass.js
@@ -3,16 +3,17 @@ Esta función hace que al hacer click ("onclick()") en el icono del ojo
 se muestre la contraseña y viceversa, ocultando la contraseña y cambiando
 de icono a un ojo cerrado.
 */
-function mostrar_pass(){
-    var tipo = document.getElementById("password");
-    var eyeIcon = document.getElementById("eye-icon");
-    if(tipo.type == 'password'){
+function mostrar_pass(fieldId, eyeIconId) {
+    var tipo = document.getElementById(fieldId);
+    var eyeIcon = document.getElementById(eyeIconId);
+
+    if (tipo.type == 'password') {
         tipo.type = 'text';
-        eyeIcon.src = "Resources/slash_eye_icon.svg"
+        eyeIcon.src = "Resources/211661_eye_icon.svg";
         eyeIcon.classList.add("closed-eye");
-    }else{
+    } else {
         tipo.type = 'password';
-        eyeIcon.src = "Resources/211661_eye_icon.svg"
+        eyeIcon.src = "Resources/slash_eye_icon.svg";
         eyeIcon.classList.remove("closed-eye");
     }
 }
